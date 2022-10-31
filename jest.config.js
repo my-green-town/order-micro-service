c155040ae1beec9761ab026273ec5b35e15e44a8
satyamk134 <satyamk134@gmail.com>
2022-10-31 15:27:43 +0530

    coverage threshold added

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 const config = {
   verbose: true,
-  collectCoverage:true
+  reporters: [ "default", "jest-junit" ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  }
 };
 module.exports = config;
